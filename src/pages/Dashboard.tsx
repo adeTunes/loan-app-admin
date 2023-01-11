21c9c416966afd6a89461b1af38c3f7dc2752735
Adekunle Babatunde <babatundeadebowale1998@gmail.com>
2023-01-11 08:23:59 -0800

    updates

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,19 @@
 import "../components/Dashboard/dashboard.scss";
 import DashboardLayout from "../components/Dashboard/DashboardLayout";
-import StatsCarousel from "../components/Dashboard/StatsCarousel";
+import StatsCard from "../components/Dashboard/StatsCard";
 import UserTable from "../components/Dashboard/UserTable";
+import adminStats from "../layout/adminStats";
 
 function Dashboard() {
   return (
     <DashboardLayout padding="0px">
-      <p className="contentHeading">Users</p>
-      <div className="contentStatsCards">
-        <StatsCarousel />
-      </div>
-      <UserTable />
+        <p className="contentHeading">Users</p>
+        <div className="contentStatsCards">
+          {adminStats.map(({ icon, name, stats, bg }, idx) => (
+            <StatsCard key={idx} icon={icon} name={name} stats={stats} bg={bg} />
+          ))}
+        </div>
+        <UserTable />
     </DashboardLayout>
   );
 }
